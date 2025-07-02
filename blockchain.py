7e37e33f2d92c90cb57ba216302e0fd681e1db5e
bradyd78 <96459960+bradyd78@users.noreply.github.com>
2025-07-02 11:14:17 -0500

    Update blockchain.py
    Add mining

--- blockchain.py
+++ blockchain.py
@@ -139,3 +139,32 @@ class Blockchain(object):
 
     if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000) 
+
+    #Add mining
+    @app.route('/mine', methods=['GET'])
+    def mine():
+        # We run the proof of work algorithm to get the next proof...
+        last_block = blockchain.last_block
+        last_proof = last_block['proof']
+        proof = blockchain.proof_of_work(last_proof)
+
+    # We must receive a reward for finding the proof.
+    # The sender is "0" to signify that this node has mined a new coin.
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    # Forge the new Block by adding it to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
